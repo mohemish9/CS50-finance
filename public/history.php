44a6e697b8ebf204a0296a5d3213468377712ac0
Mohamed Emish <34790217+mohemish9@users.noreply.github.com>
2020-01-10 14:15:12 +0200

    Update history.php

--- public/history.php
+++ public/history.php
@@ -1,21 +1,16 @@
 <?php
-
-
     require("../includes/config.php"); 
 
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-
-        $history = CS50::query("SELECT * FROM history WHERE user_id = ?", $_SESSION["id"]);
+        #stores all transactions in the log that belongs to the current user in vairable history
+        $history = CS50::query("SELECT * FROM log WHERE user_id = ?", $_SESSION["id"]);
         
-
         if(count($history) == 0) {
             apologize("There is no history for you!");
         }
-
         render("history.php", ["history" => $history, "title" => "Portfolio"]);
     }
-
     else
     {
         apologize("You can't access this page.");
