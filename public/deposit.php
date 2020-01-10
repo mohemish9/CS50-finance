4a79561abd2fc33c1d8186315df08d9fa17c0b17
Mohamed Emish <34790217+mohemish9@users.noreply.github.com>
2020-01-10 14:11:48 +0200

    Update deposit.php

--- public/deposit.php
+++ public/deposit.php
@@ -1,27 +1,18 @@
 <?php
-
-
     require("../includes/config.php"); 
 
-  
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-
         render("deposit_form.php", ["title" => "Deposit"]);
     }
-
- 
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
- 
         if (empty($_POST["amount"]))
         {
             apologize("You must provide amount");
         }
-
+        # adding the deposit amount to user's balance
         CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?", $_POST["amount"], $_SESSION["id"]);
-
         redirect("/");
     }
-
 ?>
