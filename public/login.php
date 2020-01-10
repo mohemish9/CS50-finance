e42aa662da726c71fe57cbc3e1446453955ff811
Mohamed Emish <34790217+mohemish9@users.noreply.github.com>
2020-01-10 14:21:12 +0200

    Update login.php

--- public/login.php
+++ public/login.php
@@ -1,19 +1,12 @@
 <?php
-
-    
     require("../includes/config.php"); 
-
     
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        
         render("login_form.php", ["title" => "Log In"]);
     }
-
-    
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        
+    {   
         if (empty($_POST["username"]))
         {
             apologize("You must provide your username.");
@@ -22,26 +15,19 @@
         {
             apologize("You must provide your password.");
         }
+        # fetches the username from user credentials table
         $rows = CS50::query("SELECT * FROM users WHERE username = ?", $_POST["username"]);
 
         if (count($rows) == 1)
-        {
-        
+        {   
             $row = $rows[0];
-
             
             if (password_verify($_POST["password"], $row["hash"]))
             {
-                
-                $_SESSION["id"] = $row["id"];
-
-                
+                $_SESSION["id"] = $row["id"];       
                 redirect("/");
             }
-        }
-
-        
+        }   
         apologize("Invalid username and/or password.");
     }
-
 ?>
